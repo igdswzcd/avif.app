7dfcb4e3c3aabc1a58d74b6e8b78a7bb47938b51
ZhuXiaojie <zhuxiaojie11@huawei.com>
2023-06-21 10:47:00 +0800

    v0.1

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,15 +1,15 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+use open;
+use std::io::{Read, Write};
 use std::{env, fs};
 use tauri::api::dialog::blocking::FileDialogBuilder;
+
+use crate::avif::{convert_to_avif, ConversionOptions};
+use crate::yuv::Subsampling;
 mod avif;
 mod yuv;
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
-
 #[tauri::command]
 fn select_imgs() -> Vec<String> {
     let img_paths = FileDialogBuilder::new()
@@ -17,7 +17,7 @@ fn select_imgs() -> Vec<String> {
             "Images(.png,.jpg,.jpeg,.webp,.gif,.bmp,.dib,.ico,.tiff,.tif,.pbm,.pgm,.ppm,.pnm,.dds,.tga,.icb,.vda,.vst,.ff)",
             &[
                 "png", "jpg", "jpeg", "webp", "gif", "bmp", "dib", "ico", "tiff", "tif",
-                "pbm", "pgm", "ppm", "pnm", "dds", "tga", "icb", "vda", "vst", "ff",
+                "pbm", "pgm", "ppm", "pnm", "dds", "tga", "icb", "vda", "vst", "ff"
             ],
         )
         .pick_files();
@@ -33,9 +33,67 @@ fn select_imgs() -> Vec<String> {
     }
 }
 
+#[tauri::command]
+fn get_file_size(path: String) -> u64 {
+    let metadata = fs::metadata(path).unwrap();
+    metadata.len()
+}
+
+#[tauri::command]
+fn set_save_path() -> String {
+    let save_folder = FileDialogBuilder::new().pick_folder();
+    match save_folder {
+        Some(path) => path.to_string_lossy().into_owned(),
+        None => String::new(),
+    }
+}
+
+#[tauri::command]
+fn browser_open_avif(path: String) {
+    // todo: I found that if there's only 'chrome', not working in Windows
+    // maybe here should be two tauri::command to be compatible with MacOS or others
+    open::with(&path, "start chrome").unwrap();
+}
+
+#[tauri::command]
+async fn new_cvt(
+    id: String,
+    effort: u8,
+    quality: u8,
+    transparency: bool,
+    savepath: String,
+) -> String {
+    println!(
+        "{}, {}, {}, {}, {}",
+        id, effort, quality, transparency, savepath
+    );
+    let cvt_options = ConversionOptions::new(effort, quality, Subsampling::YUV444, transparency);
+    let mut input = Vec::new();
+    fs::File::open(&id)
+        .unwrap()
+        .read_to_end(&mut input)
+        .unwrap();
+    let input = input;
+    let result = convert_to_avif(&input, &cvt_options).unwrap();
+    let pic_name = id.split("\\").last().unwrap().split(".").next().unwrap();
+    let where_to_save = format!("{savepath}\\{pic_name}.avif");
+    let mut output_file = fs::File::create(where_to_save).unwrap();
+    let cvt_res = output_file.write_all(&result);
+    match cvt_res {
+        Ok(_) => String::from("finished"),
+        Err(_) => String::from("failed"),
+    }
+}
+
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet, select_imgs])
+        .invoke_handler(tauri::generate_handler![
+            select_imgs,
+            get_file_size,
+            set_save_path,
+            browser_open_avif,
+            new_cvt
+        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/main.js
+++ src/main.js
@@ -1,36 +1,185 @@
 const { invoke } = window.__TAURI__.tauri;
 
-let greetInputEl;
-let greetMsgEl;
-let img_srcs = [];
-let img_datas = new Map();
-let default_effort = 30;
+let img_srcs = [];                // selected img_paths from rust
+let img_datas = new Map();        // unique img datas save : task_status, img_path, avif_path, quality, effort, transparency
+let default_effort = 0;
 let default_quality = 70;
 let default_transparency = true;
-async function greet() {
-  // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-  greetMsgEl.textContent = await invoke("greet", { name: greetInputEl.value });
-}
+let default_viewer = 'chrome';
+let global_save_path = null;
+let current_focused_id = null;    // id generally equals to img_path
+let running_queue = new Set();
+let waiting_queue = [];
+let parallel_limit = 6;           // limit size of running_queue
 
+/**
+ * get img_paths by rust
+ */
 async function get_imgs() {
-  let selected_srcs = await invoke("select_imgs");
+  let selected_srcs = await invoke('select_imgs');
   img_srcs.push(...selected_srcs);
-  console.log(img_srcs);
   update_table();
 }
+/**
+ * get img size by rust
+ * @param path
+ * @returns bytes in Number
+ */
+async function get_file_size(path) {
+  return await invoke('get_file_size', { path });
+}
+/**
+ * trans size in readable format
+ * @param size
+ * @returns formatted string
+ */
+function format_size(size) {
+  if (size === 0) return '0 b';
+  const k = 1024;
+  const sizes = ['b', 'kb', 'mb', 'gb', 'tb', 'pb'];
+  const i = Math.floor(Math.log(size) / Math.log(k));
+  return parseFloat((size / Math.pow(k, i)).toFixed(1)) + ' ' + sizes[i];
+}
+/**
+ * the Size <td> has no innnerText before get_file_size()
+ * update value here
+ * @param path
+ */
+async function set_original_size(path) {
+  const size = await get_file_size(path);
+  const fsize = format_size(size);
+  let tr = document.getElementById(path);
+  let td_original_size = tr.querySelector(`td.td_original_size`);
+  td_original_size.innerText = fsize;
+  img_datas.get(path).origin_size = size;
+}
+/**
+ * almost the same as set_original_size(), but avif
+ * need to provide origin image path as id to query img_datas
+ * @param id origin image path
+ * @param path avif path
+ */
+async function set_cvted_size(id, path) {
+  const size = await get_file_size(path);
+  const fsize = format_size(size);
+  let tr = document.getElementById(id);
+  let td_reslut = tr.querySelector(`td.td_result`);
+  let td_result_size = document.createElement('span');
+  td_result_size.innerText = fsize;
+  let td_result_comp = document.createElement('span');
+  const change_ratio = (
+    ((size - img_datas.get(id).origin_size) / img_datas.get(id).origin_size) *
+    100
+  ).toFixed(2);
+  td_result_comp.innerText = `(${change_ratio}%)`;
+  td_result_comp.style.color = change_ratio < 0 ? 'green' : 'red';
+  td_reslut.appendChild(td_result_size);
+  td_reslut.appendChild(td_result_comp);
+}
+/**
+ * call browser to view avif by rust
+ * in Win the cmd is 'start chrome path'
+ * todo: should change some codes in other platform
+ * @param path
+ */
+async function browser_preview(path) {
+  await invoke('browser_open_avif', { path });
+}
+/**
+ * create single convert task by rust
+ * @param id
+ */
+async function new_cvt(id) {
+  lock_params_when_running(id);
+  let cvt_result = await invoke('new_cvt', {
+    id,
+    effort: img_datas.get(id).effort,
+    quality: img_datas.get(id).quality,
+    transparency: img_datas.get(id).transparency,
+    savepath: global_save_path,
+  });
+  unlock_params(id);
+  let cur_tr = document.getElementById(id);
+  let cvt_btn = cur_tr.querySelector('#start_cvt');
+  if (cvt_result === 'finished') {
+    cvt_btn.innerText = 'DONE';
+    const avif_path =
+      global_save_path + '\\' + id.split('\\').pop().split('.')[0] + '.avif';
+    img_datas.get(id).avif_path = avif_path;
+    running_queue.delete(id);
+    set_cvted_size(id, avif_path);
+    let td_chrome = cur_tr.querySelector('td.td_chrome');
+    td_chrome.innerText = '😎';
+    td_chrome.style.cursor = 'pointer';
+    td_chrome.addEventListener('click', (event) => {
+      event.stopPropagation();
+      browser_preview(avif_path);
+    });
+  } else {
+    cvt_btn.innerText = 'FAIL';
+    cvt_btn.backgroundColor = 'lightred';
+  }
+}
+async function set_save_path() {
+  let save_path = await invoke('set_save_path');
+  if (save_path === '') {
+    return;
+  }
+  global_save_path = save_path;
+  let save_path_label = document.getElementById('save_path');
+  save_path_label.innerText = '*Save Path: ' + save_path;
+  let btn_add = document.getElementById('add_src');
+  btn_add.removeAttribute('disabled');
+}
 
+const INIT = 0,
+  WAIT = 1,
+  CVRT = 2,
+  FIN = 3,
+  FIL = 4;
+
+function lock_params_when_running(id) {
+  let tr = document.getElementById(id);
+  let td_effort_input = tr.querySelector(`td.td_effort input`);
+  td_effort_input.setAttribute('disabled', '');
+  let td_quality_input = tr.querySelector(`td.td_quality input`);
+  td_quality_input.setAttribute('disabled', '');
+  let td_transpy_select = tr.querySelector(`td.td_transpy select`);
+  td_transpy_select.setAttribute('disabled', '');
+}
+
+function unlock_params(id) {
+  let tr = document.getElementById(id);
+  let td_effort_input = tr.querySelector(`td.td_effort input`);
+  td_effort_input.removeAttribute('disabled');
+  let td_quality_input = tr.querySelector(`td.td_quality input`);
+  td_quality_input.removeAttribute('disabled');
+  let td_transpy_select = tr.querySelector(`td.td_transpy select`);
+  td_transpy_select.removeAttribute('disabled');
+}
+
+/**
+ * trigger by import pictures, add record if unique
+ */
 function update_table() {
   let img_table = document.getElementById('img_table');
   for (let item of img_srcs) {
     if (img_datas.has(item)) {
       continue;
     }
-    img_datas.set(item, { effort: 30, quality: 70, transparency: true });
+    img_datas.set(item, {
+      effort: default_effort,
+      quality: default_quality,
+      transparency: default_transparency,
+      status: INIT,
+      avif_path: null,
+      origin_size: null,
+    });
     let newTr = initTrTd(item);
     img_table.appendChild(newTr);
+    set_original_size(item);
   }
 }
-
 
 function initTrTd(item_src) {
   let item_splits = item_src.split('\\');
@@ -39,38 +188,32 @@ function initTrTd(item_src) {
   tr.id = item_src;
   let td_src = document.createElement('td');
   td_src.className = 'td_src';
-  td_src.innerText = rela_image_name.split('.')[0];
+  td_src.innerHTML = `<a>${rela_image_name.split('.')[0]}</a>`;
+  td_src.style.cursor = 'pointer';
+  td_src.addEventListener('click', (event) => {
+    event.stopPropagation();
+    browser_preview(item_src);
+  });
   let td_fmt = document.createElement('td');
   td_fmt.className = 'td_fmt';
   td_fmt.innerText = rela_image_name.split('.')[1];
-  let td_effort = document.createElement('td');
-  td_effort.className = 'td_effort';
-  td_effort.innerHTML = `<input type="range" min="0" max="100" step="5" value="${default_effort}"><span> ${default_effort}</span>`;
-  let td_quality = document.createElement('td');
-  td_quality.className = 'td_quality';
-  td_quality.innerHTML = `<input type="range" min="0" max="100" step="5" value="${default_quality}"><span> ${default_quality}</span>`;
-  let td_transpy = document.createElement('td');
-  td_transpy.className = 'td_transpy';
-  td_transpy.innerHTML = `<select><option value="keep" ${default_transparency ? 'selected' : ''}>保留</option><option value="drop" ${default_transparency ? '' : 'selected'}>不保留</option></select>`;
-  let td_cvt = document.createElement('td');
-  td_cvt.className = 'td_cvt';
-  td_cvt.innerHTML = `<button>开始</button>`;
-  let td_progress = document.createElement('td');
-  td_progress.className = 'td_progress';
-  td_progress.innerText = 'Ready...';
+  let td_original_size = document.createElement('td');
+  td_original_size.className = 'td_original_size';
+  let td_effort = initTdEffort();
+  let td_quality = initTdQuality();
+  let td_transpy = initTdTransparency();
+  let td_cvt = initTdCvt();
   let td_result = document.createElement('td');
   td_result.className = 'td_result';
-  td_result.innerText = '0kb';
   let td_chrome = document.createElement('td');
   td_chrome.className = 'td_chrome';
-  td_chrome.innerText = 'link';
   tr.appendChild(td_src);
   tr.appendChild(td_fmt);
+  tr.appendChild(td_original_size);
   tr.appendChild(td_effort);
   tr.appendChild(td_quality);
   tr.appendChild(td_transpy);
   tr.appendChild(td_cvt);
-  tr.appendChild(td_progress);
   tr.appendChild(td_result);
   tr.appendChild(td_chrome);
   tr.addEventListener('click', (event) => {
@@ -78,22 +221,137 @@ function initTrTd(item_src) {
     while (tgt.tagName !== 'TR') {
       tgt = tgt.parentNode;
     }
-    console.log(tgt);
     let rows = document.querySelectorAll('tr');
     for (let row of rows) {
       row.style.backgroundColor = '';
     }
     tgt.focus();
-    tgt.style.backgroundColor = 'green';
-  })
+    current_focused_id = tgt.id;
+    const status = img_datas.get(current_focused_id).status;
+    let btn_rm = document.getElementById('rm_src');
+    if (status === WAIT || status === CVRT) {
+      btn_rm.setAttribute('disabled', '');
+    } else {
+      btn_rm.removeAttribute('disabled');
+    }
+    tgt.style.backgroundColor = 'lightseagreen';
+  });
   return tr;
 }
 
-window.addEventListener("DOMContentLoaded", () => {
-  let btn = document.getElementById('add_src');
-  btn.addEventListener('click', () => {
+function initTdEffort() {
+  let td_effort = document.createElement('td');
+  td_effort.className = 'td_effort';
+  let td_effort_range = document.createElement('input');
+  td_effort_range.type = 'range';
+  td_effort_range.setAttribute('min', '0');
+  td_effort_range.setAttribute('max', '100');
+  td_effort_range.setAttribute('step', '5');
+  td_effort_range.setAttribute('value', default_effort);
+  let td_effort_range_span = document.createElement('span');
+  td_effort_range_span.innerText =
+    default_effort === 100 ? '100' : ' ' + default_effort;
+  td_effort_range.addEventListener('input', (event) => {
+    td_effort_range_span.innerText =
+      event.target.value === 100 ? '100' : ' ' + event.target.value;
+    const id = event.target.parentNode.parentNode.id;
+    img_datas.get(id).effort = parseInt(event.target.value);
+    reInitTdCvtBtn(id);
+  });
+  td_effort.appendChild(td_effort_range);
+  td_effort.appendChild(td_effort_range_span);
+  return td_effort;
+}
+function initTdQuality() {
+  let td_quality = document.createElement('td');
+  td_quality.className = 'td_quality';
+  let td_quality_range = document.createElement('input');
+  td_quality_range.type = 'range';
+  td_quality_range.setAttribute('min', '0');
+  td_quality_range.setAttribute('max', '100');
+  td_quality_range.setAttribute('step', '5');
+  td_quality_range.setAttribute('value', default_quality);
+  let td_quality_range_span = document.createElement('span');
+  td_quality_range_span.innerText =
+    default_quality === 100 ? '100' : ' ' + default_quality;
+  td_quality_range.addEventListener('input', (event) => {
+    td_quality_range_span.innerText =
+      event.target.value === 100 ? '100' : ' ' + event.target.value;
+    const id = event.target.parentNode.parentNode.id;
+    img_datas.get(id).quality = parseInt(event.target.value);
+    reInitTdCvtBtn(id);
+  });
+  td_quality.appendChild(td_quality_range);
+  td_quality.appendChild(td_quality_range_span);
+  return td_quality;
+}
+function initTdTransparency() {
+  let td_transpy = document.createElement('td');
+  td_transpy.className = 'td_transpy';
+  let td_transpy_select = document.createElement('select');
+  td_transpy_select.innerHTML = `<option value="keep" ${default_transparency ? 'selected' : ''
+    }>KEEP</option><option value="drop" ${default_transparency ? '' : 'selected'
+    }>DROP</option>`;
+  td_transpy_select.addEventListener('change', (event) => {
+    const id = event.target.parentNode.parentNode.id;
+    img_datas.get(id).transparency =
+      event.target.selectedIndex === 0 ? true : false;
+    reInitTdCvtBtn(id);
+  });
+  td_transpy.appendChild(td_transpy_select);
+  return td_transpy;
+}
+
+function initTdCvt() {
+  let td_cvt = document.createElement('td');
+  td_cvt.className = 'td_cvt';
+  let td_cvt_btn = document.createElement('button');
+  td_cvt_btn.setAttribute('id', 'start_cvt');
+  td_cvt_btn.innerText = 'BEGIN';
+  td_cvt_btn.addEventListener('click', (event) => {
+    img_datas.get(event.target.parentNode.parentNode.id).status = WAIT;
+    waiting_queue.push(event.target.parentNode.parentNode.id);
+    event.target.innerText = 'QUEUE';
+    event.target.setAttribute('disabled', '');
+  });
+  td_cvt.appendChild(td_cvt_btn);
+  return td_cvt;
+}
+
+function reInitTdCvtBtn(id) {
+  img_datas.get(id).status = INIT;
+  let tr = document.getElementById(id);
+  let td_cvt_btn = tr.querySelector('td.td_cvt button');
+  td_cvt_btn.removeAttribute('disabled');
+  td_cvt_btn.innerText = 'BEGIN';
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+  let btn_add = document.getElementById('add_src');
+  btn_add.addEventListener('click', () => {
     get_imgs();
-  })
+  });
+  let btn_set_save = document.getElementById('set_save');
+  btn_set_save.addEventListener('click', () => {
+    set_save_path();
+  });
+  let btn_rm = document.getElementById('rm_src');
+  btn_rm.addEventListener('click', () => {
+    if (current_focused_id === null) {
+      return;
+    }
+    const status = img_datas.get(current_focused_id).status;
+    if (status === INIT || status === FIN) {
+      img_datas.delete(current_focused_id);
+      let img_table = document.getElementById('img_table');
+      let target_child = document.getElementById(current_focused_id);
+      img_table.removeChild(target_child);
+      current_focused_id = null;
+      let btn_rm = document.getElementById('rm_src');
+      btn_rm.setAttribute('disabled', '');
+    }
+  });
+
   let dft_eft = document.getElementById('dft_eft');
   dft_eft.addEventListener('change', (event) => {
     default_effort = parseInt(event.target.value);
@@ -106,4 +364,29 @@ window.addEventListener("DOMContentLoaded", () => {
   dft_tspy.addEventListener('change', (event) => {
     default_transparency = event.target.selectedIndex === 0 ? true : false;
   });
+  let dft_parallel = document.getElementById('dft_parallel');
+  dft_parallel.addEventListener('change', (event) => {
+    parallel_limit = parseInt(event.target.value);
+  });
 });
+
+/**
+ * check waiting_queue, running_queue and limit every 500ms
+ * if ok, move tasks from waiting to running and call new_cvt()
+ */
+setInterval(() => {
+  console.log(waiting_queue.length, running_queue.size, 'aa');
+  while (waiting_queue.length > 0) {
+    if (running_queue.size < parallel_limit) {
+      let next = waiting_queue.shift();
+      running_queue.add(next);
+      img_datas.get(next).status = CVRT;
+      new_cvt(next);
+      let tr = document.getElementById(next);
+      let td_cvt_btn = tr.querySelector(`td.td_cvt button`);
+      td_cvt_btn.innerText = 'Running';
+    } else {
+      break;
+    }
+  }
+}, 500);
